a528e7652e716d25b8fa08e5b34cd94e78e14d8c
weeks4lucas <75331268+weeks4lucas@users.noreply.github.com>
2021-10-18 18:09:35 -0700

    ConstructionTester changes HowFarAway changes

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -6,5 +6,26 @@ public class HowFarAway
     public static void main(String[] args)
     {
         // your code here.
+        System.out.print("Enter the latitude of the starting location: ");
+        Scanner startLat = new Scanner(System.in);
+        double begLat = startLat.nextDouble();
+        System.out.println(startLat);
+        System.out.print("Enter the longitude of the starting location: ");
+        Scanner startLong = new Scanner(System.in);
+        double begLong = startLong.nextDouble();
+        System.out.println(startLong);
+        System.out.print("Enter the latitude of the ending location: ");
+        Scanner stopLat = new Scanner(System.in);
+        double endLat = stopLat.nextDouble();
+        System.out.println(stopLat);
+        System.out.print("Enter the longitude of the ending location: ");
+        Scanner stopLong = new Scanner(System.in);
+        double endLong = stopLong.nextDouble();
+        System.out.println(stopLong);
+
+        GeoLocation geoStart = new GeoLocation(begLat,begLong);
+        GeoLocation geoEnd = new GeoLocation(endLat,endLong);
+        double distance = geoStart.distanceFrom(geoEnd);
+        System.out.println("The distance is " + distance);
     }
 }
